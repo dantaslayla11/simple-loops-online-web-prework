fa59b20464d218b279faa81fa6b816a471c13b49
layla dantas <laylatdantas@gmail.com>
2019-05-17 03:48:25 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -25,7 +25,7 @@ def times_iterator(number_of_times)
 
 def while_iterator(number_of_times)
   
-  while  
+  while  x > 7
   puts "Welcome to Flatiron School's Web Development Course!"
   
   # code your solution here using the "while" keyword
